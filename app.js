dce996501f784bf34020075b36fde0fac1be5d2f
Joseph-K-B <303josephb@gmail.com>
2021-06-25 16:44:16 -0700

    finished functions

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const noun1 = document.getElementById("noun-1")
 const noun2 = document.getElementById("noun-2")
 const verb1 = document.getElementById("verb-1")
-const verb2 = document.getElementById("past-tense-verb")
+const pastTenseVerb = document.getElementById("past-tense-verb")
 const adjective = document.getElementById("adjective")
 
 const verb1Input = document.getElementById("verb-1-input")
@@ -13,8 +13,10 @@ const noun2Input = document.getElementById("noun-2-input")
 const noun1Btn = document.getElementById("noun-1-button");
 const noun2Btn = document.getElementById("noun-2-button");
 const verb1Btn = document.getElementById("verb-1-button");
-const verb2Btn = document.getElementById("verb-2-button");
-const adjectiveBtn = document.getElementById("adjectiv-button");
+const pastTenseVerbBtn = document.getElementById("past-tense-verb-button");
+const adjectiveBtn = document.getElementById("adjective-button");
+
+const hidden = document.getElementsByClassName('hidden');
 
 const changeText = (elem, inputField) => {
     elem.textContent = inputField.value;
@@ -26,17 +28,26 @@ noun1Btn.addEventListener('click', () => {
 })
 
 noun2Btn.addEventListener('click', () => {
-    console.log(noun1, noun1Input);
-    changeText(noun1, noun1Input);
+    console.log(noun2, noun2Input);
+    changeText(noun2, noun2Input);
 })
 
-noun1Btn.addEventListener('click', () => {
-    console.log(noun1, noun1Input);
-    changeText(noun1, noun1Input);
+
+verb1Btn.addEventListener('click', () => {
+    console.log(verb1, verb1Input);
+    changeText(verb1, verb1Input);
 })
 
-noun1Btn.addEventListener('click', () => {
-    console.log(noun1, noun1Input);
-    changeText(noun1, noun1Input);
+pastTenseVerbBtn.addEventListener('click', () => {
+    console.log(pastTenseVerb, pastTenseVerbInput);
+    changeText(pastTenseVerb, pastTenseVerbInput);
 })
 
+adjectiveBtn.addEventListener('click', () => {
+    console.log(adjective, adjectiveInput);
+    changeText(adjective, adjectiveInput);
+})
+
+noun2Btn.addEventListener('click', () => {
+    hidden[0].style.display = 'block'
+})
